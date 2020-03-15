3407b06dc69241d126029e9eb97372cc3179df61
Zoey Kim <zkim@convene.com>
2020-03-15 05:07:00 -0400

    Changed to react chartjs 2 graphs.

--- src/components/features/Graph/Graph.jsx
+++ src/components/features/Graph/Graph.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {
-    XYPlot,
-    XAxis, // Shows the values on x axis
-    YAxis, // Shows the values on y axis
-    VerticalBarSeries
-} from 'react-vis';
+// import {
+//     XYPlot,
+//     XAxis, // Shows the values on x axis
+//     YAxis, // Shows the values on y axis
+//     VerticalBarSeries
+// } from 'react-vis';
+import { Bar } from 'react-chartjs-2';
 
 import { connect } from 'react-redux';
 import { selectors as uploadsSelectors } from '../../../store/uploads';
@@ -16,7 +17,7 @@ import './Graph.css';
 
 const Graph = ({ processedCSV }) => {
     const [graphCategory, setGraphCategory] = React.useState(null);
-    const xValues = ['Business', 'Entertainment', 'Groceries', 'Merchandise', 'Miscellaneous', 'Restaurant', 'Supplies', 'Transportation', 'Travel'];
+    const x = ['Business', 'Entertainment', 'Groceries', 'Merchandise', 'Miscellaneous', 'Restaurant', 'Supplies', 'Transportation', 'Travel'];
 
     let currentTotal = 0;
     let filteredCards = graphCategory && processedCSV[0].spendings.filter((p) => p.Category === graphCategory.x);
@@ -25,41 +26,98 @@ const Graph = ({ processedCSV }) => {
         return <Card key={f.Reference} data={f} />
     });
 
-    let graphObject = processedCSV[0].spendings.map((d) => {
-        const xValue = xValues.find(c => c === d.Category);
-        return {
-            'x': xValue,
-            'y': Number(d.Amount)
+    const data = () => {
+        let businessTotal = 0;
+        let entertainmentTotal = 0;
+        let groceriesTotal = 0;
+        let merchandiseTotal = 0;
+        let miscTotal = 0;
+        let restaurantTotal = 0;
+        let suppliesTotal = 0;
+        let transportationTotal = 0;
+        let travelTotal = 0;
+
+        for (const spending of processedCSV[0].spendings) {
+            switch (spending.Category) {
+                case 'Business':
+                    businessTotal += Number(spending.Amount);
+                    break;
+                case 'Entertainment':
+                    entertainmentTotal = entertainmentTotal += Number(spending.Amount);
+                    break;
+                case 'Groceries':
+                    groceriesTotal = groceriesTotal += Number(spending.Amount);
+                    break;
+                case 'Merchandise':
+                    merchandiseTotal = merchandiseTotal += Number(spending.Amount);
+                    break;
+                case 'Miscellaneous':
+                    miscTotal = miscTotal += Number(spending.Amount);
+                    break;
+                case 'Restaurant':
+                    restaurantTotal = restaurantTotal += Number(spending.Amount);
+                    break;
+                case 'Supplies':
+                    suppliesTotal = suppliesTotal += Number(spending.Amount);
+                    break;
+                case 'Transportation':
+                    transportationTotal = transportationTotal += Number(spending.Amount);
+                    break;
+                case 'Travel':
+                    travelTotal = travelTotal += Number(spending.Amount);
+                    break;
+                default:
+                    miscTotal = miscTotal += Number(spending.Amount);
+                    break;
+            }
         }
-    });
+        const values = [businessTotal, entertainmentTotal, groceriesTotal, merchandiseTotal, miscTotal, restaurantTotal, suppliesTotal, transportationTotal, travelTotal];
 
-    const chartWidth = 900;
-    const chartHeight = 750;
-    const chartDomain = [0, chartHeight];
 
-    const onSeriesClick = React.useCallback((datapoint) => {
-        setGraphCategory(datapoint);
-    }, [setGraphCategory]);
+        return {
+            labels: x,
+            datasets: [
+                {
+                    label: 'Amount',
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.6)',
+                        'rgba(54, 162, 235, 0.6)',
+                        'rgba(255, 206, 86, 0.6)',
+                        'rgba(75, 192, 192, 0.6)',
+                        'rgba(153, 102, 255, 0.6)',
+                        'rgba(255, 159, 64, 0.6)',
+                        'rgba(255, 99, 132, 0.6)',
+                        'rgba(255, 0, 0, 0.8)',
+                        'rgba(0, 255, 0, 0.3)'
+                    ],
+                    borderColor: 'rgba(0,0,0,1)',
+                    borderWidth: 2,
+                    data: values.flat()
+                }
+            ]
+        }
+    };
 
-    console.log('ggg', graphObject);
+    // const onSeriesClick = React.useCallback((datapoint) => {
+    //     setGraphCategory(datapoint);
+    // }, [setGraphCategory]);
 
     return (
         <div className="graphDiv">
-            <XYPlot
-                xType="ordinal"
-                width={chartWidth}
-                height={chartHeight}
-                yDomain={chartDomain}
-                className="graphDiv-xyplot"
-            >
-                <XAxis />
-                <YAxis tickTotal={4} />
-                <VerticalBarSeries
-                    data={graphObject}
-                    color="green"
-                    onValueClick={onSeriesClick}
-                />
-            </XYPlot>
+            <p className="graphDiv-total">TOTAL: ${processedCSV[0].total}</p>
+            <Bar 
+                data={data}
+                options={{
+                    title: {
+                        display: true,
+                        text: 'Your Spendings',
+                        fontSize: 25
+                    },
+                    legend: {
+                        display: false
+                    }
+                }}
+            />
             <div className="graphDiv-categorized">
                 {
                     graphCategory &&
--- src/components/features/LineGraph/LineGraph.jsx
+++ src/components/features/LineGraph/LineGraph.jsx
@@ -1,40 +1,69 @@
 import React from 'react';
-import {
-    XYPlot,
-    XAxis,
-    YAxis,
-    LineSeries,
-    VerticalGridLines,
-    HorizontalGridLines
-} from 'react-vis';
+
+import { Line } from 'react-chartjs-2';
 
 import './LineGraph.css';
 
 const LineGraph = ({ files }) => {
     console.log('inside line graph', files);
 
-    let i = 1;
-    const data = files && files.map(f => {
-        console.log('fff', f);
-        f.id = i;
-        i++;
-        return {
-            'x': f.id,
-            'y': f.total
+    const data = () => {
+        let i = 1;
+        const totalLabels = [];
+        const dataValues = [];
+        for (const file of files) {
+            file.id = i;
+            i++;
+            if (file.id) {
+                totalLabels.push(file.id);
+                dataValues.push(file.total);
+            }
         }
-    });
+        
+        return {
+            labels: totalLabels.flat(),
+                datasets: [
+                    {
+                        label: 'Amount',
+                        fill: false,
+                        lineTension: 0,
+                        backgroundColor: 'rgba(75,192,192,0.4)',
+                        borderColor: 'rgba(75,192,192,1)',
+                        borderCapStyle: 'butt',
+                        borderDash: [],
+                        borderDashOffset: 0.0,
+                        borderJoinStyle: 'miter',
+                        pointBorderColor: 'rgba(75,192,192,1)',
+                        pointBackgroundColor: '#fff',
+                        pointBorderWidth: 1,
+                        pointHoverRadius: 5,
+                        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                        pointHoverBorderColor: 'rgba(220,220,220,1)',
+                        pointHoverBorderWidth: 2,
+                        pointRadius: 1,
+                        pointHitRadius: 10,
+                        data: dataValues.flat()
+                    }
+                ]
+        }
+    };
 
     return (
         <div className="lineGraphDiv">
             <br />
             { files ?
-                <XYPlot className="lineGraphDiv-xyPlot" height={400} width={400}>
-                    <VerticalGridLines />
-                    <HorizontalGridLines />
-                    <XAxis tickValues={[0, 1, 3, 4, 5]} />
-                    <YAxis />
-                    <LineSeries data={data} />
-                </XYPlot>
+                <Line
+                    data={data}
+                    options={{
+                        title: {
+                            display: true,
+                            text: 'Your Spendings Trend'
+                        },
+                        legend: {
+                            display: false
+                        }
+                    }}
+                />
             : null}
         </div>
     );
